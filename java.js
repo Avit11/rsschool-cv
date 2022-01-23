7004548c5b1e0959a4568e63ca9a47ef05fc52f4
Avit11 <97055203+Avit11@users.noreply.github.com>
2022-01-23 12:11:11 +0300

    create java.js

---
+++ java.js
@@ -0,0 +1,14 @@
+public class DataHolder {
+    private String data;
+
+    public DataHolder() {
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+}
